07461c6e46adb8aed9c65d6a04916153e78126d4
eri-Practice <64237498+eri-Practice@users.noreply.github.com>
2020-07-01 15:59:07 +0900

    デバイス準備１（gemの追加など）

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'toppage/index'
-  root "toppage#index"
+  # root "toppage#index"
+  root 'items#index'
   resources :items
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_07_01_014315) do
 
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "introduction", null: false
     t.integer "price", null: false
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_07_01_014315) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "tests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "tests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
