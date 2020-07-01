712f1bd0563796eb041a224fd4e53ff5e8db21ea
eri-Practice <64237498+eri-Practice@users.noreply.github.com>
2020-07-01 21:16:30 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
-end
-# test
+
   before_action :basic_auth, if: :production?
 
   private
