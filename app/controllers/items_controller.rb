9bc6ff086f85444ffb85a5adc9dfe2e55fcf3b0b
eri-Practice <64237498+eri-Practice@users.noreply.github.com>
2020-07-03 13:06:00 +0900

    フォームの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,23 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def new
+    @item = Item.new 
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to new_item_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :category, :seller, :buyer, :condition, :postage_payer, :prefecture, :standby_day).merge(seller: current_user.id)
+  end
+
+
 end
