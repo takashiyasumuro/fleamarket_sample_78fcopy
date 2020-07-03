af296e5c6fb25c391bba5e38826dce42bf4afa6c
eri-Practice <64237498+eri-Practice@users.noreply.github.com>
2020-07-03 11:48:21 +0900

    カラムの追加

--- db/migrate/20200701014315_create_items.rb
+++ db/migrate/20200701014315_create_items.rb
@@ -1,9 +1,9 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :name, null: false
-      t.text :introduction, null: false
-      t.integer :price, null: false
+      t.string :name
+      t.text :introduction
+      t.integer :price
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_070840) do
+ActiveRecord::Schema.define(version: 2020_07_03_021312) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "introduction", null: false
-    t.integer "price", null: false
+    t.string "name"
+    t.text "introduction"
+    t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category"
+    t.integer "seller"
+    t.integer "buyer"
+    t.string "condition"
+    t.string "postage_payer"
+    t.string "prefecture"
+    t.string "standby_day"
   end
 
   create_table "tests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
